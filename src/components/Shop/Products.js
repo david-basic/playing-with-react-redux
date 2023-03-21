ab92a3b9b5cfb7d04408d4375079388a6b890e38
David Bašić <d.basic97.db@gmail.com>
2023-03-22 00:48:33 +0100

    wiring up components

--- src/components/Shop/Products.js
+++ src/components/Shop/Products.js
@@ -1,19 +1,51 @@
-import ProductItem from './ProductItem';
-import classes from './Products.module.css';
+import ProductItem from "./ProductItem";
+import classes from "./Products.module.css";
+
+const PRODUCT_MOCK = [
+	{
+		id: "p1",
+		price: 6,
+		title: "My first book",
+		description: "This is a first product - amazing!",
+	},
+
+	{
+		id: "p2",
+		price: 7,
+		title: "My second book",
+		description: "This is a second product - amazing!",
+	},
+	{
+		id: "p3",
+		price: 8,
+		title: "My third book",
+		description: "This is a thrid product - amazing!",
+	},
+	{
+		id: "p4",
+		price: 9,
+		title: "My fourth book",
+		description: "This is a fourth product - amazing!",
+	},
+];
 
 const Products = (props) => {
-  return (
-    <section className={classes.products}>
-      <h2>Buy your favorite products</h2>
-      <ul>
-        <ProductItem
-          title='Test'
-          price={6}
-          description='This is a first product - amazing!'
-        />
-      </ul>
-    </section>
-  );
+	return (
+		<section className={classes.products}>
+			<h2>Buy your favorite products</h2>
+			<ul>
+				{PRODUCT_MOCK.map((product) => (
+					<ProductItem
+						key={product.id}
+						id={product.id}
+						title={product.title}
+						price={product.price}
+						description={product.description}
+					/>
+				))}
+			</ul>
+		</section>
+	);
 };
 
 export default Products;
