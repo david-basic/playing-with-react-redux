0e9bd1b0e741e5c053654ca209d71c5721795d60
David Bašić <d.basic97.db@gmail.com>
2023-03-22 21:02:48 +0100

    action creator, redux done

--- src/store/cart-slice.js
+++ src/store/cart-slice.js
@@ -2,14 +2,19 @@ import { createSlice } from "@reduxjs/toolkit";
 
 const cartSlice = createSlice({
 	name: "cart",
-	initialState: { items: [], totalQuantity: 0 },
+	initialState: { items: [], totalQuantity: 0, change: false },
 	reducers: {
+		replaceCart(state, action) {
+			state.totalQuantity = action.payload.totalQuantity;
+			state.items = action.payload.items;
+		},
 		addItemToCart(state, action) {
 			const newItem = action.payload;
 			const existingItem = state.items.find(
 				(item) => item.id === newItem.id
 			);
 			state.totalQuantity++;
+			state.change = true;
 
 			if (!existingItem) {
 				state.items.push({
@@ -29,6 +34,7 @@ const cartSlice = createSlice({
 			const id = action.payload;
 			const existingItem = state.items.find((item) => item.id === id);
 			state.totalQuantity--;
+			state.change = true;
 
 			if (existingItem.quantity === 1) {
 				state.items = state.items.filter((item) => item.id !== id);
